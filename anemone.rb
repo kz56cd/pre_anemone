41fcd4de709269e458e744dcf03fbeb7a273dcfe
kz56cd <kz56cd@gmail.com>
2015-03-12 06:00:42 +0900

    testing anemone

--- anemone.rb
+++ anemone.rb
@@ -1,3 +1,14 @@
 require 'anemone'
 
-puts 'hey'
+# puts 'hey'
+
+Anemone.crawl("http://news.yahoo.co.jp/", :depth_limit => 0) do |anemone|
+	anemone.on_every_page do |page|
+		# page.doc.xpath("//*[@id=¥"editorsPack¥"]/div/ul[1]/li/div/p/a").each do |title|
+		# page.doc.xpath("//*[@id=\"editorsPack\"]/div/ul[1]/li/div/p/a").each do |title|
+			# puts title.text
+		# end
+
+		puts page.doc
+	end
+end
